7d38ad317ad68c4bf9b1fb3bffb52216b29dfbe4
sanadee_2004 <sanadeehaputhanthire@gmail.com>
2025-10-05 09:55:13 +0530

    added new code

---
+++ src/Demo14.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Demo14 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        double[]salary = new double[8];
+
+        for(int i=0; i<8; i++){
+            System.out.print("Enter your Salary:");
+            salary[i] = scan.nextDouble();
+        }
+        double[]new_salary = new double[8];
+        for(int i=0; i<8; i++){
+            if(salary[i]>130000){
+                new_salary[i] = (salary[i]*12/100)+salary[i];
+            }else{
+                new_salary[i]=salary[i];
+            }
+        }
+        double total =0;
+        for (int i=0; i<8;i++){
+            total += salary[i];
+        }
+
+        double over_salary=0;
+        for(int i=0; i<8;i++){
+            over_salary += new_salary[i];
+        }
+        System.out.println("over cost is "+(total-over_salary) );
+    }
+}
